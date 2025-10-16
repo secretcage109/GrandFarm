0b2ed16e1ce770c0dd68ca302bdd93c58154016b
cheon0911 <cheon0911@gmail.com>
2025-10-16 16:05:20 +0900

    SolveProblem
    저장 로드했을때 인벤토리에 있는 아이템은 메인 레벨에 등장하지 않음
    
    월광 추가

--- Source/TestMap/PlantActor.cpp
+++ Source/TestMap/PlantActor.cpp
@@ -59,7 +59,7 @@ void APlantActor::UpdateGrowthByTime(float DeltaTime)
         ElapsedTime += DeltaTime * GrowthRate;
 
     // 하루 = 86400초 기준 → 10초 기준 예시
-        float DaysPassed = ElapsedTime / 20.f;
+        float DaysPassed = ElapsedTime / 245.f;
 
         if (DaysPassed >= 3.f)
         {
